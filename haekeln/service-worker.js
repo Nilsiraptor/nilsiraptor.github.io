1beb401f2e5b73da487a9d0474c7b2009584bf6a
Nils Liebreich <nilsl99@mi.fu-berlin.de>
2026-01-26 15:58:51 +0100

    Updated service-worker

--- haekeln/service-worker.js
+++ haekeln/service-worker.js
@@ -1,25 +1,26 @@
-const CACHE_NAME = "haekel-tracker-v6";
+const CACHE_NAME = "haekel-tracker-v7";
 const ASSETS = [
-  "./index.html",
-  "./style.css",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "../Lexend.ttf"
+    "./",
+    "./index.html",
+    "./style.css",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png",
+    "../Lexend.ttf"
 ];
 
 // Installieren und Dateien cachen
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
 // Anfragen abfangen: Erst Cache, dann Netzwerk
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
